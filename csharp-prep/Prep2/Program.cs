9d5264ccfa0a849f581f44ea056f40b3cb80c54c
simphwe-nkabinde-byui <nka22001@byui.edu>
2023-01-10 03:11:49 +0200

    W01 Prove: Developer—C# Programming Part 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeStringValue = Console.ReadLine();
+        int gradePercent = int.Parse(gradeStringValue);
+        string letter;
+
+        if (gradePercent >= 90) 
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Letter grade: {letter}");
+
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations!, You passed");
+        }
+        else 
+        {
+            Console.WriteLine("You have failed, Better luck next time");
+        }
     }
 }
