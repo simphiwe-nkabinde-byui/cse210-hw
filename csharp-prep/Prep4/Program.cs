b3240563ac5ce84041922dac5273c5024be5609a
simphwe-nkabinde-byui <nka22001@byui.edu>
2023-01-12 10:17:04 +0200

    W02 Prove: Developer—C# Programming Parts 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        int num =  -1;
+        int max = 0;
+        
+        while (num != 0)
+        {
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num != 0)
+            {
+                numbers.Add(num);          
+            }
+
+        }
+
+        foreach (int item in numbers)
+        {
+            sum += item;
+            if (item > max) 
+            {
+                max = item;
+            } 
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum / numbers.Count}");
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
