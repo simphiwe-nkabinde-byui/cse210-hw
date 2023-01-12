cb57d3d50d3f0d2ab83a1dff10d4840a801c1757
simphwe-nkabinde-byui <nka22001@byui.edu>
2023-01-12 10:48:37 +0200

    W02 Prove: Developer—C# Programming Part 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        
+        int square = SquareNumber(num);
+
+        DisplayResult(name, square);
+    }
+
+    static void DisplayWelcome() 
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName() 
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, int squaredNum)
+    {
+        Console.WriteLine($"Brother {name}, the square of your number is {squaredNum}");
     }
 }
